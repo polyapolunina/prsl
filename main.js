c60ae9fe06d0c3dce2fdd3bd0adcecc45c8e4fca
Владимир Заболотских <m30b34735@mail.ru>
2022-07-15 23:24:52 +0300

    Событие дял скрытия меню допилить

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const burger = document.getElementById('burger')
+const cross = document.getElementById('cross')
 const menu = document.getElementById('menu')
+const menuItem = document.getElementById('menuItem')
 
 burger.onclick = () => {
     // menu.className = "menuFalse"
@@ -7,6 +9,21 @@ burger.onclick = () => {
         menu.className = "menuShow"
     }
     else if (menu.className == "menuShow") {
+        menu.className = "menu"
+    }
+}
+cross.onclick = () => {
+    // menu.className = "menuFalse"
+    if (menu.className == "menu") {
+        menu.className = "menuShow"
+    }
+    else if (menu.className == "menuShow") {
+        menu.className = "menu"
+    }
+}
+menuItem.onclick = () => {
+    // menu.className = "menuFalse"
+    if (menu.className == "menuShow") {
         menu.className = "menu"
     }
 }
